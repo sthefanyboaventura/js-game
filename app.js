a3542483cc4a6d2cc4964739afbc5daae8dee165
Sthefany Boaventura <sthefanysantos818@gmail.com>
2024-02-14 16:17:33 -0300

    número secreto

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerText = 'Jogo do número secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -14,5 +9,10 @@ exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('botão clicado')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
