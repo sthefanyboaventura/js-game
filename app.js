2f8f35479407eda9059eeac057960e08e22e6d9f
Sthefany Boaventura <sthefanysantos818@gmail.com>
2024-02-14 15:44:44 -0300

    evoluindo código

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerText = 'Jogo do número secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerText = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-let campo = document.querySelector(tag);
-campo.innerHTML = texto
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     console.log('botão clicado')
