4958ae5b08be3d6feb0441a25264475a0a4b1f05
Sthefany Boaventura <sthefanysantos818@gmail.com>
2024-02-14 16:42:42 -0300

    if else

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-}
 
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número é maior');
+        }
+    }
+}    
+    
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
