a675bb08ec56202e4774ab72db53bf8641d4977e
Sthefany Boaventura <sthefanysantos818@gmail.com>
2024-02-14 17:37:10 -0300

    novo jogo

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -25,10 +29,10 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número é maior');
         }
         tentativas++;
-        limparCampo(); 
+        limparCampo();
     }
-}    
-    
+}
+
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
@@ -36,4 +40,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
 }
