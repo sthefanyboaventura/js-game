8f0e2e1cb6ab4ae9a41b7c7980e50aeafca41f91
Sthefany Boaventura <sthefanysantos818@gmail.com>
2024-02-14 15:36:13 -0300

    alteração de funções

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ titulo.innerText = 'Jogo do número secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let campo = document.querySelector(tag);
+campo.innerHTML = texto
+
 function verificarChute() {
     console.log('botão clicado')
 }
